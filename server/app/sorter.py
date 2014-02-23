754a9aab0bded0e8072714f501d0c8e41efb94da
Nick Firmani <nick.firmani@berkeley.edu>
2014-02-23 04:36:27 -0800

    Adding one more file

--- server/app/sorter.py
+++ server/app/sorter.py
@@ -15,12 +15,13 @@ def get_suggestions(pint_obj, pinid, desc, purl)):
     #now we know the obj exists, so lets find some suggestions
     currsug = list(pint_obj.suggestions.all())
     listwords = rake(desc)
-    while len(currsug) < 5 && i < len(listwords):
+    while len(currsug) < 5 && i < len(listwords)-1:
         i = 0
-        vals = macy_search(listwords[i]);
+        vals = macy_search(listwords[i] + listwords[i+1]);
         if vals[0] != 0: 
             #this means we got data back
             k = 1
+            #create new suggestion obj
             while vals[k] != None && k < 16:
                 s = models.suggestion(pintrest_id=pint_obj,picture,product_title=vals[k],
                     product_URL=vals[k+1], picture_URL=vals[k+2])
@@ -28,8 +29,14 @@ def get_suggestions(pint_obj, pinid, desc, purl)):
                 k += 3
         i += 1
     if len(currsug) == 0:
+        #we have no suggestions :(
         return None
-    #sort suggestions    
-    return currsug
+    weights = [5,4,3,2,1]
+    int k = 0
+    for(k in currsug):
+        weight = (k.user_rating * 5 + k.mturk_rating * 3 + weights[k] * 2)/30
+        weights[k] = weight
+        k += 1 #REORDER FIXME BY WEIGHT
+    return currsug 
     
     
