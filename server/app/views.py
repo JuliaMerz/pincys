2cefb304e884bec4587b4cd6d301fea496b6c364
Nick Firmani <nick.firmani@berkeley.edu>
2014-02-23 00:32:23 -0800

    More server fun :)

--- server/app/views.py
+++ server/app/views.py
@@ -1,19 +1,28 @@
-from app import app, db
+from app import app, db, models, sorter
 from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
     return '<meta http-equiv="refresh" content="0; url=https://github.com/SebastianMerz/pincys" />'
 
-@app.route('/get_suggestions/<pin_id>', methods=['POST'])
-def process_input(pin_id):
+@app.route('/get_suggestions/', methods=['POST'])
+def process_input():
     #basically, call the db on pin_id, find the suggestions that result, sort them then 
     #if there are no suggestions, search macys api
+    
+    #get info from the request
     pinid = request.form['ID']
     desc = request.form['DESCRIPTION']
     
-    return 'HTMLGOESHERE'
+    #get item in the db or none if its not there
+    picobj = models.pintrest_picture.query.filter_by(pintrest_ID=pinid).first()
+    
+    #get the suggestion objects etc
+    sorted_sugg_list = get_suggestions(picobj)
+    
+    #convert it to html and return it (sebastian do this)
+    return get_html(sorted_sugg_list)
 
 @app.route('/add_turksuggest/')
 def addTS():
-    return "NOT IMPLEMENTED YET"
+    return "Not implemented."
