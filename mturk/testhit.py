118222f4e5bb059d88d19bebe2c4774a7137bc0c
Nick Firmani <nick.firmani@berkeley.edu>
2014-02-22 18:05:48 -0800

    Started work on the flask server

--- mturk/testhit.py
+++ mturk/testhit.py
@@ -1,5 +1,5 @@
 from boto.mturk.connection import MTurkConnection
-from boto.pyami.config import Config
+from keys import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 
 HOST = 'mechanicalturk.sandbox.amazonaws.com'
