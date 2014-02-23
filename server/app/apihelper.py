ed27cb4d4b215516c65a1312a01c73ebda3ba5e0
Nick Firmani <nick.firmani@berkeley.edu>
2014-02-23 02:42:41 -0800

    More work on api

---
+++ server/app/apihelper.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from flask import request
+from keys import APIKEY
+
+#returns top 5 picurls/producturl/producttitle for the designated keyword
+def macySearch(key):
+    hdr = {
+        "Accept": "application/json",
+        "X-Macys-Webservice-Client-Id": APIKEY,
+    }
+    
+    url = 'http://api.macys.com/v4/catalog/search?searchphrase=' + key +
+    '&show=product&perpage=5&imagewidth=262&avoidredirects=true'
+    resp = requests.get(url ,headers=hdr)
+    app.logger.debug(resp.status_code)
+    jdata = json.loads(resp.text)
+    
+    num_products = jdata["searchresultgroups"][0]["totalproducts"]
+    app.logger.debug(num_products)
+    if num_products == 0:
+        return None
+    
+    products = jdata["searchresultgroups"][0]["products"]['product']
+    
+    retArr = range(15)
+    k = 0
+    for i in products:
+        retArr[k] = i["summary"]["name"]
+        k += 1
+        retArr[k] = i["summary"]["producturl"]
+        k += 1
+        retArr[k] = i["image"][0]["imageurl"]
+        k += 1
+    return retArr
+    
+    
+    
+    
